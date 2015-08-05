91410d2d5f852592ce3e5a14fed6f2eb74c6bee6
tanyi <tanyimailcn@gmail.com>
2015-08-05 23:59:24 +0800

    add all

---
+++ decode.py
@@ -0,0 +1,11 @@
+#字符串和编码
+
+print('',ord('A'),'\n',
+ord('中'),'\n',
+chr(66),'\n',
+chr(20013),'\n',
+'ABC'.encode('ascii'),'\n',
+'中文'.encode('utf-8'),'\n',
+len('ABC'),'\n',
+len('中文'.encode('utf-8')),'\n',)
+
